25d321afefa4be3d4e268e8e425ab0e5e791b673
nicoasp <nicolas.amaya@gmail.com>
2017-05-18 18:02:01 +0200

    super substantial contributions

--- server.js
+++ server.js
@@ -9,4 +9,10 @@ const QUANDL_API_KEY = process.env.QUANDL_API_KEY
 api_key=${QUANDL_API_KEY}
 &start_date=2017-05-01&end_date=2017-05-18`
 
-`https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?date=20160912&qopts.columns=ticker,date,close&api_key=${QUANDL_API_KEY}`
+`https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?
+date=20160912&qopts.columns=ticker,date,close&
+api_key=${QUANDL_API_KEY}`
+
+
+Displaying stocks for 1 day 
+requires same stocks for 1day, 7days, 30days before
