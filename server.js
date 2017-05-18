7dc9fcd2c11c15529417620b1da65404fbbc592d
nicoasp <nicolas.amaya@gmail.com>
2017-05-18 19:02:05 +0200

    finish date lib

--- server.js
+++ server.js
@@ -21,22 +21,22 @@ function checkStatus(response) {
   return response;
 }
 
-const dateBuilder = (date) => {
-  const dayOfWeek = Date.new(date).getDay();
-  return {
-    current:
-  }
-}
+
+const {
+  dateBuilder,
+  datesStringBuilder
+} = require('./lib/dateBuilder');
 
 app.get('/api/stocks', (req, res, next) => {
   const date = req.query.date;
   const dates = dateBuilder(date);
+  const datesString = datesStringBuilder(dates);
 
   fetch(`${baseUrl}?date=${dates}&qopts.columns=ticker,date,close` +
         `&api_key=${QUANDL_API_KEY}`)
 });
 
 
-`https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?
-date=20160912,20160911&qopts.columns=ticker,date,close&
-api_key=${QUANDL_API_KEY}`
+// `https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?
+// date=20160912,20160911&qopts.columns=ticker,date,close&
+// api_key=${QUANDL_API_KEY}`
